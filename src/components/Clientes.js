694765965d112cfdaa7c0310527569b9949291ac
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2017-10-10 16:07:05 -0300

    Prototipacao inicial da tela de Clientes.

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react'
 
+import { 
+  Form, Button, 
+  Divider,
+  Table, Icon
+} from 'semantic-ui-react'
+
 export default class Clientes extends Component {
 
   render() {
     return (
       <div>
         <h1>Listagem de Clientes</h1>
+
+        <Form size='small'>
+          <Form.Group widths='equal'>
+            <Form.Input label='Nome' placeholder='Nome do Cliente' />
+          </Form.Group>
+          <Button color='blue' size='small' style={{width: 90}}>Buscar</Button>
+          <Button color='blue' size='small' style={{width: 90}}>Limpar</Button>
+          <Button color='green' size='small' style={{width: 90}}>Adicionar</Button>
+        </Form>
+
+        <Divider />
+
+        <Table celled striped compact size='small'>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Nome</Table.HeaderCell>
+              <Table.HeaderCell>Ações</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Maria</Table.Cell>
+              <Table.Cell collapsing textAlign='left'>
+                <Button color='blue' size='small' icon>
+                  <Icon name='edit' />
+                </Button>
+                <Button color='red' size='small' icon>
+                  <Icon name='remove' />
+                </Button>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+
       </div>
     )
   }
