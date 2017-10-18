a3be2a59578aa18f8ff22ef75d9dd775973b314e
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2017-10-18 17:17:48 -0200

    feat: adiciona form modal para cadastro de imovel.

--- src/components/Imoveis.js
+++ src/components/Imoveis.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import { 
   Form, Button, 
-  Divider,
+  Divider, Modal, Header, Image,
   Table, Icon
 } from 'semantic-ui-react'
 
@@ -15,7 +15,8 @@ export default class Imoveis extends Component {
     tiposImovel: [],
     situacoesImovel: [],
     cidades: [],
-    estados: []
+    estados: [],
+    createModalVibible: false
   }
 
   componentDidMount() {
@@ -34,6 +35,8 @@ export default class Imoveis extends Component {
     this.setState({ tiposImovel, situacoesImovel, cidades, estados })
   }
 
+  toggleCreateModalVisibility = () => this.setState({ createModalVibible: !this.state.createModalVibible })
+
   render() {
     const { tiposImovel, situacoesImovel, cidades, estados } = this.state
 
@@ -43,14 +46,14 @@ export default class Imoveis extends Component {
 
         <Form size='small'>
           <Form.Group widths='equal'>
-            <Form.Input label='Proprietário' placeholder='Nome do proprietátio do Imóvel' />
+            <Form.Select label='Proprietário' search placeholder='Nome do proprietátio do Imóvel' />
             <Form.Select label='Tipo de Imóvel' placeholder='Selecione o Tipo de Imóvel' options={tiposImovel} />
             <Form.Select label='Situação do Imóvel' placeholder='Selecione a Situação do Imóvel' options={situacoesImovel} />
           </Form.Group>
 
           <Form.Group widths='equal'>
-            <Form.Select label='Cidade' placeholder='Selecione a Cidade do Imóvel' options={cidades} />
-            <Form.Select label='Estado' placeholder='Selecione a Estado do Imóvel' options={estados} />
+            <Form.Select label='Cidade' search placeholder='Selecione a Cidade do Imóvel' options={cidades} />
+            <Form.Select label='Estado' search placeholder='Selecione a Estado do Imóvel' options={estados} />
           </Form.Group>
 
           <Form.Group widths='equal'>
@@ -62,7 +65,7 @@ export default class Imoveis extends Component {
 
           <Button color='blue' size='small' style={{width: 90}}>Buscar</Button>
           <Button color='blue' size='small' style={{width: 90}}>Limpar</Button>
-          <Button color='green' size='small' style={{width: 90}}>Adicionar</Button>
+          <Button color='green' size='small' style={{width: 90}} onClick={this.toggleCreateModalVisibility}>Adicionar</Button>
         </Form>
 
         <Divider />
@@ -70,7 +73,7 @@ export default class Imoveis extends Component {
         <Table celled striped compact size='small'>
           <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Foto</Table.HeaderCell>
+              <Table.HeaderCell>Foto Principal</Table.HeaderCell>
               <Table.HeaderCell width={3}>Proprietário</Table.HeaderCell>
               <Table.HeaderCell width={2}>Tipo de Imóvel</Table.HeaderCell>
               <Table.HeaderCell width={2}>Situação Atual</Table.HeaderCell>
@@ -83,13 +86,13 @@ export default class Imoveis extends Component {
 
           <Table.Body>
             <Table.Row>
-              <Table.Cell></Table.Cell>
+              <Table.Cell><Image src='img/imovel01.jpg' size='medium' /></Table.Cell>
               <Table.Cell>Maria</Table.Cell>
               <Table.Cell>Casa</Table.Cell>
               <Table.Cell>Venda</Table.Cell>
-              <Table.Cell>Florianopolis/SC</Table.Cell>
-              <Table.Cell>250.000</Table.Cell>
-              <Table.Cell>100</Table.Cell>
+              <Table.Cell>Florianópolis/SC</Table.Cell>
+              <Table.Cell>R$ 250.000</Table.Cell>
+              <Table.Cell>100 m²</Table.Cell>
               <Table.Cell collapsing textAlign='left'>
                 <Button color='blue' size='small' icon>
                   <Icon name='edit' />
@@ -102,6 +105,64 @@ export default class Imoveis extends Component {
           </Table.Body>
         </Table>
 
+
+        <Modal 
+          size='large' dimmer
+          open={this.state.createModalVibible} 
+          onClose={this.toggleCreateModalVisibility}
+        >
+          <Modal.Header>Cadastro de um novo Imóvel</Modal.Header>
+          <Modal.Content scrolling>
+            <Form size='small'>
+              <Form.Select label='Proprietário' search placeholder='Cliente e Proprietário do Imóvel' />
+              
+              <Form.Input type="file" label='Foto Principal do Imóvel' />
+
+              <Form.Group widths='equal'>
+                <Form.Select label='Tipo de Imóvel' placeholder='Tipo de Imóvel' options={tiposImovel} />
+                <Form.Select label='Situação do Imóvel' placeholder='Situação do Imóvel' options={situacoesImovel} />
+                <Form.Input label='Área Total em m²' placeholder='Área Total do Imóvel em m²' />
+              </Form.Group>
+              
+              <Form.Group widths='equal'>
+                <Form.Input label='Valor R$' placeholder='Valor' />
+                <Form.Input label='Valor do IPTU R$' placeholder='Valor do IPTU' />
+                <Form.Input label='Valor do Condomínio R$' placeholder='Valor do Condomínio' />
+              </Form.Group>
+
+              <Form.Group widths='equal'>
+                <Form.Input label='Número de Quartos' placeholder='Numero de Quartos' />
+                <Form.Input label='Número de Banheiros' placeholder='Número de Banheiros' />
+                <Form.Input label='Vagas na Garagem' placeholder='Número de Vagas na Garagem' />
+              </Form.Group>
+
+              <Divider />
+              
+              <Header size='medium'>Endereço do Imóvel</Header>
+              <Form.Group>
+                <Form.Input label='CEP' placeholder='CEP' width={4} />
+                <Form.Input label='Rua' placeholder='Rua' width={6} />
+                <Form.Input label='Bairro' placeholder='Bairro' width={6} />
+              </Form.Group>
+              <Form.Group>
+                <Form.Input label='Número' placeholder='Número' width={4} />
+                <Form.Input label='Complemento' placeholder='Complemento' width={6} />
+              </Form.Group>
+              <Form.Group widths='equal'>
+                <Form.Select label='Estado' search placeholder='Estado' options={estados} />
+                <Form.Select label='Cidade' search placeholder='Cidade' options={cidades} />
+              </Form.Group>
+
+              <Divider />
+
+              <Form.TextArea label='Descrição' placeholder='Informações Gerais sobre o Imóvel...' style={{ minHeight: 100 }} />
+            </Form>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color='red' onClick={this.toggleCreateModalVisibility}>Cancelar</Button>
+            <Button color='blue' onClick={this.toggleCreateModalVisibility}>Salvar</Button>
+          </Modal.Actions>
+        </Modal>
       </div>
     )
   }
