5846d151ce67f04c539aab226e29d0f0f3c18015
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2017-10-30 16:07:56 -0200

    refactor: melhora os campos de imobiliaria.

--- src/components/Imobiliarias.js
+++ src/components/Imobiliarias.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
 
 import { 
-  Form, Button, 
+  Form, Input, Button, 
   Divider, Header, Modal,
   Table, Icon, Image
 } from 'semantic-ui-react'
 
+import CidadesAPI from '../api/CidadesAPI'
+
 export default class Imobiliarias extends Component {
 
   state = {
-    createModalVibible: false
+    createModalVibible: false,
+    estados: [],
+    cidades: []
+  }
+
+  componentDidMount() {
+    const estados = CidadesAPI.getEstados()
+      .map((e) => ({ key: e.id, text: e.nome, value: e.id }))
+
+    const cidades = CidadesAPI.getCidades()
+      .map((e) => ({ key: e.id, text: e.nome, value: e.id }))
+
+    this.setState({ estados, cidades })
   }
 
   toggleCreateModalVisibility = () => this.setState({ createModalVibible: !this.state.createModalVibible })
 
   render() {
+    const { estados, cidades } = this.state
+
     return (
       <div>
         <h1>Listagem de Imobiliárias</h1>
@@ -22,12 +38,16 @@ export default class Imobiliarias extends Component {
         <Form size='small'>
           <Form.Group widths='equal'>
             <Form.Input label='Razão Social' placeholder='Razão Social da Imobiliária' />
-            <Form.Input label='CNPJ' placeholder='CNPJ da Imobiliária' />
+
+            <Form.Field>
+              <label>CNPJ</label>
+              <Input label='#' placeholder='99.999.999/9999-99' />
+            </Form.Field>
           </Form.Group>
 
           <Form.Group widths='equal'>
-            <Form.Input label='Estado' placeholder='Estado' />
-            <Form.Input label='Cidade' placeholder='Cidade do endereço da Imobiliária' />
+            <Form.Select label='Estado' placeholder='Estado' search options={estados} />
+            <Form.Select label='Cidade' placeholder='Cidade do endereço da Imobiliária' search options={cidades} />
           </Form.Group>
 
           <Button color='blue' size='small' style={{width: 90}}>Buscar</Button>
@@ -43,6 +63,7 @@ export default class Imobiliarias extends Component {
               <Table.HeaderCell width={2}>Logo</Table.HeaderCell>
               <Table.HeaderCell width={4}>Razão Social</Table.HeaderCell>
               <Table.HeaderCell width={2}>CNPJ</Table.HeaderCell>
+              <Table.HeaderCell width={2}>COFECI</Table.HeaderCell>
               <Table.HeaderCell>Cidade da Sede</Table.HeaderCell>
               <Table.HeaderCell>Ações</Table.HeaderCell>
             </Table.Row>
@@ -52,8 +73,9 @@ export default class Imobiliarias extends Component {
             <Table.Row>
               <Table.Cell><Image src='img/ibagy-logo.png' size='tiny' /></Table.Cell>
               <Table.Cell>IBagy</Table.Cell>
-              <Table.Cell>111.111.111-11</Table.Cell>
-              <Table.Cell>Florianopolis/SC</Table.Cell>
+              <Table.Cell>99.999.999/9999-99</Table.Cell>
+              <Table.Cell>2500</Table.Cell>
+              <Table.Cell>Florianópolis/SC</Table.Cell>
               <Table.Cell collapsing textAlign='left'>
                 <Button color='blue' size='small' icon>
                   <Icon name='edit' />
@@ -75,26 +97,42 @@ export default class Imobiliarias extends Component {
           <Modal.Content scrolling>
             <Form size='small'>
               <Form.Group widths='equal'>
-                <Form.Input label='Razão Social' placeholder='Razão Social da Imobiliária' />
-                <Form.Input label='CNPJ' placeholder='CNPJ da Imobiliária' />
+                <Form.Input required label='Razão Social' placeholder='Razão Social da Imobiliária' />
+                
+                <Form.Field required>
+                  <label>CNPJ</label>
+                  <Input label='#' placeholder='99.999.999/9999-99' />
+                </Form.Field>
+
+                <Form.Field required>
+                  <label>COFECI</label>
+                  <Input placeholder='0' />
+                </Form.Field>
               </Form.Group>
+
               <Form.Input type="file" label='Logo da Imobiliária' />
 
               <Divider />
 
               <Header size='medium'>Endereço da sede</Header>
               <Form.Group>
-                <Form.Input label='CEP' placeholder='CEP' width={4} />
+                <Form.Field required width={4}>
+                  <label>CEP</label>
+                  <Input label='#' placeholder='99.999-999' />
+                </Form.Field>
+
                 <Form.Input label='Rua' placeholder='Rua' width={6} />
                 <Form.Input label='Bairro' placeholder='Bairro' width={6} />
               </Form.Group>
+
               <Form.Group>
-                <Form.Input label='Número' placeholder='Número' width={4} />
+                <Form.Input label='Número' placeholder='0' width={4} />
                 <Form.Input label='Complemento' placeholder='Complemento' width={6} />
               </Form.Group>
+
               <Form.Group widths='equal'>
-                <Form.Input label='Estado' placeholder='Estado' />
-                <Form.Input label='Cidade' placeholder='Cidade' />
+                <Form.Select label='Estado' placeholder='Estado' search options={estados} />
+                <Form.Select label='Cidade' placeholder='Cidade' search options={cidades} />
               </Form.Group>
             </Form>
           </Modal.Content>
